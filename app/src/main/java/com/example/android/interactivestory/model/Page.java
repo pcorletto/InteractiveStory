46ca420cfd9a744a84db28341af93f8e38f38cd6
Peter Corletto <peter.corletto@gmail.com>
2015-11-27 14:24:31 -0500

    Finished the UI for StoryActivity

--- app/src/main/java/com/example/android/interactivestory/model/Page.java
+++ app/src/main/java/com/example/android/interactivestory/model/Page.java
@@ -9,6 +9,33 @@ public class Page {
     private String mText;
     private Choice mChoice1;
     private Choice mChoice2;
+    private Boolean mIsFinal = false;
+
+    public Page(int imageId, String text, Choice choice1, Choice choice2){
+
+        mImageId = imageId;
+        mText = text;
+        mChoice1 = choice1;
+        mChoice2 = choice2;
+    }
+
+    public Boolean getIsFinal() {
+        return mIsFinal;
+    }
+
+    public void setIsFinal(Boolean isFinal) {
+        mIsFinal = isFinal;
+    }
+
+    public Page (int imageId, String text){
+
+        mImageId = imageId;
+        mText = text;
+        mChoice1 = null;
+        mChoice2 = null;
+        mIsFinal = true;
+
+    }
 
     public int getImageId(){
         return mImageId;
