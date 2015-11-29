d447f299b25ff337a694aee7d83809dce7928b8f
Peter Corletto <peter.corletto@gmail.com>
2015-11-28 21:23:52 -0500

    Loaded the first story page into the StoryActivity layout

--- app/src/main/java/com/example/android/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/example/android/interactivestory/ui/StoryActivity.java
@@ -1,34 +1,68 @@
 package com.example.android.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.android.interactivestory.R;
+import com.example.android.interactivestory.model.Page;
+import com.example.android.interactivestory.model.Story;
 
 
 public class StoryActivity extends ActionBarActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
 
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1, mChoice2;
+    private String mName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        //mImageView = (ImageView) findViewById(R.id.storyImageView);
+        //mTextView = (TextView) findViewById(R.id.storyTextView);
+        //mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        //mChoice2 = (Button) findViewById(R.id.choiceButton2);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
-        if(name == null){
-            name = "Friend";
+        mName = intent.getStringExtra(getString(R.string.key_name));
+        if(mName == null){
+            mName = "Friend";
         }
 
-        Log.d(TAG, name);
-
+        Log.d(TAG, mName);
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+        loadPage();
 
     }
 
+    private void loadPage(){
+        Page page = mStory.getPage(0);
 
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = page.getText();
+        //Add the name if placeholder included. Won't add if no placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
+
+    }
 
 
 
